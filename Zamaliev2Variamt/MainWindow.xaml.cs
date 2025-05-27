52c488629137f396b0b4d63ed913f15fc20f34f3
212311 <212311@1220-010-22>
2025-05-27 16:45:19 +0300

    Добавьте файлы проекта.

---
+++ Zamaliev2Variamt/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zamaliev2Variamt
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Imt_Click(object sender, RoutedEventArgs e)
+        {
+            double rost = double.Parse(Height.Text);
+            double mass = double.Parse(Mass.Text);
+            if (rost <= 0)
+            {
+                MessageBox.Show("Введите корректно ваш рост");
+            }
+            else if (mass <= 0)
+            {
+                MessageBox.Show("Введите корректно вашу массу");
+            }
+            else
+            {
+                double imt = mass / (rost *= rost);
+                MessageBox.Show("Ваш ИМТ: " + imt);
+            }
+        }
+    }
+}
---
+++ Zamaliev2Variant/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamaliev2Variant
+{
+    class Program
+    {
+        static int GetMax(int[] mass)
+        {
+            int max = mass[0];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] > max)
+                {
+                    max = mass[i];
+                }
+            }
+            return max;
+        }
+
+        static int SumEven(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count += array[i];
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 2, 5, 6, 8, 9 };
+            Console.WriteLine("Наибольшое значение из массива = " + GetMax(array));
+            int[] arrayChetnie = { 2, 5, 6, 1, 1 };
+            Console.WriteLine("Сумма всех четных чисел в массиве = " + SumEven(arrayChetnie));
+        }
+    }
+}
